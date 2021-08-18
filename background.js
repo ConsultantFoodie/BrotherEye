85dd00e67aad31715e99f8fde18516cba78620b6
ConsultantFoodie <hardikti@gmail.com>
2021-08-19 00:15:20 +0530

    Test extension

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
+// background.js
+
+let color = '#3aa757';
+
 chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({color: '#3aa757'}, function() {
     console.log("The color is green.");
