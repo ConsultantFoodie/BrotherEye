32a18ac837524209d15cecdfee3286bc042511aa
ConsultantFoodie <hardikti@gmail.com>
2021-08-25 00:08:54 +0530

    Adds toggle button

--- background.js
+++
@@ -1,9 +0,0 @@
-// background.js
-
-let color = '#3aa757';
-
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log("The color is green.");
-  });
-});
--- popup.js
+++ popup.js
@@ -1,17 +1,33 @@
 // When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
+toggleButton.addEventListener("change", async (event) => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setPageBackgroundColor,
-  });
+  if(toggleButton.checked){
+	chrome.scripting.executeScript({
+		target: { tabId: tab.id },
+		function: setCheck,
+	  });
+  }
+  else{
+	chrome.scripting.executeScript({
+		target: { tabId: tab.id },
+		function: setUncheck,
+	  });
+  }
+  
 });
 
 // The body of this function will be executed as a content script inside the
 // current page
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
-  });
+function setCheck() {
+	var button = document.createElement("Button");
+	button.id = "MyButton";
+	button.innerHTML = "Title";
+	button.style = "top:0;right:0;position:absolute;z-index: 9999"
+	document.body.appendChild(button);
+}
+
+function setUncheck() {
+	var button = document.getElementById("MyButton");
+	button.remove();
 }
