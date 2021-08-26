6d5cd758bc76890d53bc5ea86ef545d9c2fdc23f
ConsultantFoodie <hardikti@gmail.com>
2021-08-26 20:58:20 +0530

    Adds test event for button click

--- popup.js
+++ popup.js
@@ -1,3 +1,28 @@
+document.addEventListener("DOMContentLoaded", async () => {
+	chrome.storage.sync.get(["isChecked"], (result) => {
+		toggleButton.checked = result.isChecked;
+	});
+	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+	chrome.scripting.executeScript({
+		target: { tabId: tab.id },
+		function: addPanel,
+	});
+});
+
+function addPanel(){
+	var panel = document.createElement("div");
+	var heading = document.createElement("div");
+	heading.setAttribute("class", "CYZUZd");
+	heading.innerHTML = '<div class="J8vCN zHGix" role="heading" aria-level="2" tabindex="-1" jsname="rQC7Ie" id="c15">BrotherEye</div>';
+	panel.appendChild(heading);
+	panel.setAttribute("class", "WUFI9b qdulke");
+	panel.setAttribute("data-tab-id","7");
+	panel.setAttribute("jsname", "b0t70b");
+	panel.setAttribute("jscontroller","dkJU2d");
+	panel.setAttribute("jsaction","VOcP9c:QPhnyd;ntQuZe:EuYDs");
+	// var panelLocation = document.querySelector("#ow3 > div.T4LgNb > div > div:nth-child(9) > div.crqnQb > div.R3Gmyc.qwU8Me");
+	// panelLocation.insertBefore(panel, panelLocation.childNodes[1]);
+}
 // When the button is clicked, inject setPageBackgroundColor into current page
 toggleButton.addEventListener("change", async (event) => {
 	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
@@ -36,10 +61,11 @@ function setCheck() {
 	button.setAttribute("aria-label","My Button");
 	button.setAttribute("data-tooltip-enabled","true");
 	button.setAttribute("data-tooltip-id","tt-c6");
-	button.setAttribute("data-panel-id","2");
+	button.setAttribute("data-panel-id","7");
 	button.setAttribute("aria-pressed","false");
 	button.setAttribute("data-id","o3HEdc");
 	button.innerHTML = "BTP";
+	button.addEventListener("click", () =>{alert("Test button")});
 	mainSpan.appendChild(button);
 	mainDiv.appendChild(mainSpan);
 	var panelButtons = document.querySelector("#ow3 > div.T4LgNb > div > div:nth-child(9) > div.crqnQb > div.rG0ybd.xPh1xb.P9KVBf > div.TqwH9c > div.SZfyod > div > div")
